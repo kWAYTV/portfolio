04c07644afdbcb1e07b4aa1b65111839c7a5a747
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-06 04:23:06 +0200

    Changed titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'perc.dev',
+    template: '%s | perc.dev',
   },
   description: 'This is my portfolio.',
   openGraph: {
