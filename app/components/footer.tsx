545800bb709781a60db9c698fa361f25f5c6b054
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-06 04:31:28 +0200

    Updated footer and nav links to underline when hovered

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             href="/rss"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7 hover:underline">rss</p>
           </a>
         </li>
         <li>
@@ -38,7 +38,7 @@ export default function Footer() {
             href="https://github.com/kWAYTV"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7 hover:underline">github</p>
           </a>
         </li>
         <li>
@@ -49,7 +49,7 @@ export default function Footer() {
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">template</p>
+            <p className="ml-2 h-7 hover:underline">template</p>
           </a>
         </li>
       </ul>
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -26,7 +26,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
