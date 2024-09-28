68d013e017729c7b23af590d5d3d04c741850cea
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-28 22:01:10 +0200

    Updated description on rss route

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -30,9 +30,9 @@ export async function GET() {
     const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
     <channel>
-        <title>My Portfolio</title>
+        <title>perc.dev</title>
         <link>${baseUrl}</link>
-        <description>This is my portfolio RSS feed</description>
+        <description>perc.dev RSS feed</description>
         ${itemsXml}
     </channel>
   </rss>`;
