c2bb89dc8e69d061150e7c40b1823a799238889c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-28 21:56:12 +0200

    Formatted

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
-import { baseUrl } from 'app/sitemap'
+import { baseUrl } from "app/sitemap";
 
 export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-  }
+    return {
+        rules: [
+            {
+                userAgent: "*",
+            },
+        ],
+        sitemap: `${baseUrl}/sitemap.xml`,
+    };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,17 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getBlogPosts } from "app/blog/utils";
 
-export const baseUrl = 'https://perc.dev'
+export const baseUrl = "https://perc.dev";
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
+    let blogs = getBlogPosts().map((post) => ({
+        url: `${baseUrl}/blog/${post.slug}`,
+        lastModified: post.metadata.publishedAt,
+    }));
 
-  let routes = ['', '/blog'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+    let routes = ["", "/blog"].map((route) => ({
+        url: `${baseUrl}${route}`,
+        lastModified: new Date().toISOString().split("T")[0],
+    }));
 
-  return [...routes, ...blogs]
+    return [...routes, ...blogs];
 }
