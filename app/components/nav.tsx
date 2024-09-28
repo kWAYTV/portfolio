f4b187b671a89536b87a13b9337fb142c98d230a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-28 18:38:06 +0200

    Use the icons imported from it's own components

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,52 @@
-import Link from 'next/link'
+import Link from "next/link";
+import React from "react";
+import { HomeIcon, GitHubIcon, BlogIcon } from "app/components/icons";
 
 const navItems = {
-  '/': {
-    name: 'home',
-  },
-  'https://github.com/kWAYTV/portfolio': {
-    name: 'github',
-  },
-  '/blog': {
-    name: 'blog',
-  },
-}
+    "/": {
+        name: "home",
+        icon: HomeIcon,
+    },
+    "https://github.com/kWAYTV": {
+        name: "github",
+        icon: GitHubIcon,
+    },
+    "/blog": {
+        name: "blog",
+        icon: BlogIcon,
+    },
+};
 
 export function Navbar() {
-  return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline flex align-middle relative py-1 px-2 m-1"
+    return (
+        <aside className="-ml-[8px] mb-16 tracking-tight">
+            <div className="lg:sticky lg:top-20">
+                <nav
+                    className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+                    id="nav"
                 >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
-  )
+                    <div className="flex flex-row space-x-0 pr-10">
+                        {Object.entries(navItems).map(
+                            ([path, { name, icon: Icon }]) => {
+                                return (
+                                    <Link
+                                        key={path}
+                                        href={path}
+                                        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex items-center relative py-1 px-2 m-1 group"
+                                    >
+                                        <span className="mr-2 w-4 h-4 inline-flex items-center justify-center">
+                                            <Icon />
+                                        </span>
+                                        <span className="group-hover:underline">
+                                            {name}
+                                        </span>
+                                    </Link>
+                                );
+                            }
+                        )}
+                    </div>
+                </nav>
+            </div>
+        </aside>
+    );
 }
