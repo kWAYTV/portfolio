8bdfbfaf50414168c238ba6fef3090617474f68f
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-10-02 08:32:50 +0200

    Deleted trailing projects page files

--- app/blog/utils.ts
+++ app/blog/utils.ts
@@ -55,9 +55,11 @@ export function getBlogPosts() {
 
 export function formatDate(date: string, includeRelative = false) {
     let currentDate = new Date();
+
     if (!date.includes("T")) {
         date = `${date}T00:00:00`;
     }
+
     let targetDate = new Date(date);
 
     let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
