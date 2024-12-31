409968327763d2602b3122ae424af5e476ea1b13
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-12-31 14:58:26 +0100

    fix(description): update text to reflect (neo)Vim enthusiasm

--- src/components/core/home/description.tsx
+++ src/components/core/home/description.tsx
@@ -10,8 +10,8 @@ export function Description() {
       transition={{ duration: 0.5, delay: 0.2 }}
       className='mb-4'
     >
-      {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-      Vim's keystroke commands and tabs' flexibility for personal viewing
+      {`I'm a (neo)Vim enthusiast and tab advocate, finding unmatched efficiency in
+      (neo)Vim's keystroke commands and tabs' flexibility for personal viewing
       preferences. This extends to my support for static typing, where its
       early error detection ensures cleaner code, and my preference for dark
       mode, which eases long coding sessions by reducing eye strain.`}
