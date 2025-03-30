121fe9adb11264b1a0bf00533b9a4f4332ca0abb
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-03-31 00:54:17 +0200

    fix(hero): update hero title to reflect personal branding
    - Changed hero title from "My Portfolio" to "Martín" for a more personalized touch.

--- src/components/core/home/hero.tsx
+++ src/components/core/home/hero.tsx
@@ -1,7 +1,5 @@
 export function Hero() {
   return (
-    <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-      My Portfolio
-    </h1>
+    <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>Martín</h1>
   );
 }
