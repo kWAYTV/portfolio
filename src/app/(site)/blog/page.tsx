0a6bf210b6b73e41b0c63b90fa1d760e912fc3bd
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-03-31 00:11:34 +0200

    fix(page): remove Suspense and skeleton loading for BlogPage
    Updated BlogPage component to directly render BlogPosts without Suspense or skeleton loading, enhancing performance and simplifying the code.

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -1,31 +1,10 @@
-import { Suspense } from 'react';
-
 import { BlogPosts } from '@/components/core/blog/posts';
-import { Skeleton } from '@/components/ui/skeleton';
 
 export default function BlogPage() {
   return (
     <section>
       <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>My Blog</h1>
-      <Suspense fallback={<BlogPostsSkeleton />}>
-        <BlogPosts />
-      </Suspense>
+      <BlogPosts />
     </section>
-  );
-}
-
-function BlogPostsSkeleton() {
-  return (
-    <div className='my-8 space-y-2'>
-      {[1, 2, 3, 4, 5].map(i => (
-        <div key={i} className='py-2'>
-          <div className='flex gap-2'>
-            <Skeleton className='h-5 w-[100px]' />
-            <Skeleton className='h-5 w-full' />
-          </div>
-          <Skeleton className='mt-2 h-px w-full' />
-        </div>
-      ))}
-    </div>
   );
 }
