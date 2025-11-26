5484edda00414cdde63941ba6a98bf00ff8c29fe
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-11-26 03:29:43 +0100

    feat: Refactor ThemeToggle component to use next-themes for theme management and update state handling

--- src/components/theming/toggle.tsx
+++ src/components/theming/toggle.tsx
@@ -1,25 +1,31 @@
 "use client";
 
 import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 import { useState } from "react";
-
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 
 const ThemeToggle = () => {
-  const [checked, setChecked] = useState<boolean>(true);
+  const [isDark, setIsDark] = useState<boolean>(true);
+  const { setTheme } = useTheme();
+
+  const handleThemeChange = () => {
+    setIsDark(!isDark);
+    setTheme(isDark ? "light" : "dark");
+  };
 
   return (
     <div className="inline-flex items-center gap-2">
       <Switch
         aria-label="Toggle switch"
-        checked={checked}
+        checked={isDark}
         id="icon-label"
-        onCheckedChange={setChecked}
+        onCheckedChange={handleThemeChange}
       />
       <Label htmlFor="icon-label">
         <span className="sr-only">Toggle switch</span>
-        {checked ? (
+        {isDark ? (
           <MoonIcon aria-hidden="true" className="size-4" />
         ) : (
           <SunIcon aria-hidden="true" className="size-4" />
