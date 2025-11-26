e07d6292a375adab5cf26b0cdcb2a39c90c45223
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-11-26 03:07:10 +0100

    feat: Update Next.js configuration to disable dev indicators and enable component caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  cacheComponents: true,
 };
 
 export default nextConfig;
