9469950034ec7da4f3d0a6d60b34e6c0191e5cd2
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-11-26 02:47:51 +0100

    feat: Integrate Toaster component for notifications in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theming/provider";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
           enableSystem
         >
           {children}
+          <Toaster richColors />
         </ThemeProvider>
       </body>
     </html>
