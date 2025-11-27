ce5a5dd4a2723981af74f361ab8836b5bb9e3b2b
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-11-27 21:44:39 +0100

    chore(footer): add "use client" directive to footer component

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export function Footer() {
   return (
     <footer className="-translate-x-1/2 absolute bottom-5 left-1/2 flex items-center gap-2 text-[10px] text-muted-foreground/40 sm:bottom-8 sm:text-xs">
