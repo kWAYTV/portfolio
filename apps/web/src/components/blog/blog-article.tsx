cd66f251624ce6c88f7861e5399bf5820b2ea7c1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2026-02-23 23:59:03 +0100

    fix(blog): update BlogArticle component styling for improved list item visibility
    - Enhanced the styling of the BlogArticle component by adding a specific text color for list items to improve readability and visual consistency.

--- apps/web/src/components/blog/blog-article.tsx
+++ apps/web/src/components/blog/blog-article.tsx
@@ -8,7 +8,7 @@ interface BlogArticleProps {
 
 export function BlogArticle({ MDX, components }: BlogArticleProps) {
   return (
-    <article className="prose prose-neutral prose-sm dark:prose-invert prose-table:block prose-table:w-full max-w-none prose-img:max-w-full prose-pre:overflow-x-auto prose-table:overflow-x-auto prose-img:rounded-md prose-headings:font-medium prose-a:text-foreground prose-code:text-xs prose-p:text-muted-foreground/80 prose-headings:tracking-tight prose-a:underline-offset-4">
+    <article className="prose prose-neutral prose-sm dark:prose-invert prose-table:block prose-table:w-full max-w-none prose-img:max-w-full prose-pre:overflow-x-auto prose-table:overflow-x-auto prose-img:rounded-md prose-headings:font-medium prose-a:text-foreground prose-code:text-xs prose-p:text-muted-foreground/80 prose-headings:tracking-tight prose-a:underline-offset-4 prose-li:[&>p]:text-foreground">
       <MDX components={components} />
     </article>
   );
